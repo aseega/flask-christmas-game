d39e96651a38a7b012a00b6ae215e2bef6d07146
aseega <156912117+aseega@users.noreply.github.com>
2024-12-11 09:25:00 +0000

    test flask

--- api/index.py
+++ api/index.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    x = 1
+    y = 2
+    if x = y:
+        return 'Hello, World!'
+    else:
+        return x
 
 @app.route('/about')
 def about():
     return 'About'
+10
